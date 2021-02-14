91937b901a25f149b83adafea8fefdeb0931826c
DevHyperCoder <arduinoleo88@gmail.com>
2021-02-14 19:17:32 +0530

    Dracula theme

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#ffb86c", "#282a36" },
+	[SchemeSel] = { "#50da7b", "#44476a" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
